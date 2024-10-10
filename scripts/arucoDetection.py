cbbaf6d58510f497e83132aff9fe3aa63884a1bb
Cursed Entertainment <110344485+CursedPrograms@users.noreply.github.com>
2024-10-10 19:32:40 +0200

    ...

--- scripts/arucoDetection.py
+++ scripts/arucoDetection.py
@@ -1,9 +1,6 @@
 import numpy as np
-import cv2
-import sys
 import time
-
-
+import cv2
 
 
 ARUCO_DICT = {
@@ -30,8 +27,8 @@ ARUCO_DICT = {
 	"DICT_APRILTAG_36h11": cv2.aruco.DICT_APRILTAG_36h11
 }
 
+
 def aruco_display(corners, ids, rejected, image):
-    
 	if len(corners) > 0:
 		
 		ids = ids.flatten()
@@ -63,30 +60,6 @@ def aruco_display(corners, ids, rejected, image):
 
 
 
-def pose_estimation(frame, aruco_dict_type, matrix_coefficients, distortion_coefficients):
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    aruco_dict = cv2.aruco.Dictionary_get(aruco_dict_type)  # Changed to use local variable
-    parameters = cv2.aruco.DetectorParameters_create()
-
-    corners, ids, rejected_img_points = cv2.aruco.detectMarkers(
-        gray, aruco_dict, parameters=parameters,
-        cameraMatrix=matrix_coefficients,
-        distCoeff=distortion_coefficients
-    )
-
-    if len(corners) > 0:
-        # Draw detected markers
-        cv2.aruco.drawDetectedMarkers(frame, corners, ids)
-
-        for i in range(len(ids)):
-            rvec, tvec, _ = cv2.aruco.estimatePoseSingleMarkers(corners[i], 0.02, matrix_coefficients, distortion_coefficients)
-
-            # Draw the axis for each marker
-            cv2.aruco.drawAxis(frame, matrix_coefficients, distortion_coefficients, rvec, tvec, 0.01)
-
-    return frame
-
-    
 
 aruco_type = "DICT_5X5_100"
 
@@ -95,28 +68,31 @@ arucoDict = cv2.aruco.Dictionary_get(ARUCO_DICT[aruco_type])
 arucoParams = cv2.aruco.DetectorParameters_create()
 
 
-intrinsic_camera = np.array(((933.15867, 0, 657.59),(0,933.1586, 400.36993),(0,0,1)))
-distortion = np.array((-0.43948,0.18514,0,0))
-
-
 cap = cv2.VideoCapture(0)
 
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
 
-
 while cap.isOpened():
     
-    ret, img = cap.read()
-    
-    output = pose_estimation(img, ARUCO_DICT[aruco_type], intrinsic_camera, distortion)
+	ret, img = cap.read()
 
-    cv2.imshow('Estimated Pose', output)
+	h, w, _ = img.shape
 
-    key = cv2.waitKey(1) & 0xFF
-    if key == ord('q'):
-        break
+	width = 1000
+	height = int(width*(h/w))
+	img = cv2.resize(img, (width, height), interpolation=cv2.INTER_CUBIC)
+ 
+	corners, ids, rejected = cv2.aruco.detectMarkers(img, arucoDict, parameters=arucoParams)
 
-cap.release()
+	detected_markers = aruco_display(corners, ids, rejected, img)
+
+	cv2.imshow("Image", detected_markers)
+
+	key = cv2.waitKey(1) & 0xFF
+	if key == ord("q"):
+	    break
+
 cv2.destroyAllWindows()
+cap.release()
